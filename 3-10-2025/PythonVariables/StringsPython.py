3c6af5a39776478621a7c92227fc8e1305cb7611
beherasurya <beherasuryakanta18@gmail.com>
2025-10-04 11:22:19 +0530

    python basics

---
+++ 3-10-2025/PythonVariables/StringsPython.py
@@ -0,0 +1,8 @@
+# How to create Strings in Python
+
+word1 = 'Apple' # Creating String by using single quote
+word2 = "Banana" #creating String by using double quote
+word3 = ''' A man who eats an apple everyday 
+will stay fit for long time''' #Creating string by using triple quote for multiline characters
+
+print(word1,word2,word3)
