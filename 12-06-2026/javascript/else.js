7fdb90c45e400deda5f347c40b75b021e79197af
beherasurya <beherasuryakanta18@gmail.com>
2026-06-28 11:07:50 +0530

    ecomdata

---
+++ 12-06-2026/javascript/else.js
@@ -0,0 +1,8 @@
+let text;
+if(Math.random() <0.5){
+    text= "<a href = 'https://w3schools.com'> Visit W3Schools</a>";
+} else{
+    text = "<a href = 'https://wwf.org'>Visit WWF</a>";
+
+}
+document.getElementById("demo").innerHTML=text;
---
+++ 12-06-2026/javascript/table.js
@@ -0,0 +1,6 @@
+
+let table1 = [
+    {name : "John", age:30},
+    {name: "Jane", age:35}
+];
+console.table(table1);
